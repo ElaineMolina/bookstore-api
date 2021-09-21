73801caceb379d90157e7cae0a4da61aedc0606f
ElaineMolina <elainemolina10@gmail.com>
2021-09-21 10:20:32 -0300

    finalizando as classes de domínio

--- src/main/java/com/molina/bookstore/domin/Categoria.java
+++ src/main/java/com/molina/bookstore/domin/Categoria.java
@@ -1,16 +1,30 @@
 package com.molina.bookstore.domin;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-
-	public Integer getId() {
-		return id;
-	}
+	private String descricao;
+	
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Livro> livros = new ArrayList();
 
 	public Categoria(Integer id, String nome, String descricao) {
 		super();
@@ -19,9 +33,10 @@ public class Categoria {
 		this.descricao = descricao;
 	}
 
-	private String descricao;
+	
+	
+	
 
-	private List<Livro> livros = new ArrayList();
 
 	public Categoria() {
 		super();
@@ -43,7 +58,9 @@ public class Categoria {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	public Integer getId() {
+		return id;
+	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
