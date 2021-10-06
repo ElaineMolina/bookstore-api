a35d0c8945c3df4a874e8e3603a7c99775a15759
ElaineMolina <elainemolina10@gmail.com>
2021-10-06 09:47:05 -0300

    atualizando

---
+++ src/main/java/com/molina/bookstore/service/CategoriaService.java
@@ -0,0 +1,53 @@
+package com.molina.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.molina.bookstore.domain.Categoria;
+import com.molina.bookstore.dtos.CategoriaDTO;
+import com.molina.bookstore.repositories.CategoriaRepository;
+import com.molina.bookstore.service.exceptions.DataIntegrityViolationException;
+import com.molina.bookstore.service.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow	(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:  " + id + ",  Tipo:  " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (org.springframework.dao.DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Categoria não pode ser deletada! Possui livros associados");
+		}
+		
+	}
+}
